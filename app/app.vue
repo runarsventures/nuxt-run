feb5d39d736b44b6862ed1835880a8fc524c5e05
runarsventures <runarsventures@gmail.com>
2025-07-23 07:24:01 +0000

    Initialized NuxtRun starter kit

---
+++ app/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <NuxtRouteAnnouncer />
+    <div class="text-2xl font-bold">
+      Welcome to NuxtRun
+    </div>
+  </div>
+</template>
